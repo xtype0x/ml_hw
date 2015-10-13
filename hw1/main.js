d263444f20696439d5a16c0fd70063f085c93fb7
type0 <ghyghygg30@gmail.com>
2015-10-13 14:37:45 +0800

    problem 16~19

--- hw1/main.js
+++ hw1/main.js
@@ -1,9 +1,14 @@
 var fs = require("fs");
 var my_pla = require("./pla");
+var ProgressBar = require("progress");
+var _ = require("underscore");
+
+var plotly = require('plotly')("type0", "w177ogc10w");
 
 var trainfile;
-var X = [];
-var y = [];
+var test_data
+var X = [], test_X = [];
+var y = [], test_y = [];
 
 //read parameter
 var argv = require('optimist').options('problem',{default:""}).argv;
@@ -16,9 +21,30 @@ if(allow_problem_set.indexOf(argv.problem) == -1){
 	return process.exit(0);
 }
 switch(argv.problem){
+	case 18:
+	case 19:
+	case 20:
+		trainfile = "hw1_18_train.dat";
+		//load test data
+		var test_data = fs.readFileSync("hw1_18_test.dat","utf8");
+		lines = test_data.split("\n");
+		for(i=0;i<lines.length;i++){
+			if(!lines[i])continue;
+			spl = lines[i].split("\t");
+			x = spl[0].split(" ").map(function(item){
+				return parseFloat(item)
+			});
+			x.unshift(1); //add x0 = 1
+			test_X.push(x);
+			test_y.push(parseInt(spl[1]));
+		}
+		break;
 	case 15:
+	case 16:
+	case 17:
 	default:
 		trainfile = "hw1_15_train.dat";
+		break;
 }
 
 //load data
@@ -35,10 +61,88 @@ for(i=0;i<lines.length;i++){
 	y.push(parseInt(spl[1]));
 }
 
-//run my pla
+//solve problem ><
+var bar = new ProgressBar('Progress:[:bar] :percent, Left::etas',{
+	complete: '=',
+	incomplete: ' ',
+	width: 20,
+	total: 2000
+});
 switch(argv.problem){
 	case 15:
-		my_pla.naive_cycle(X,y);
+		result = my_pla.naive_cycle(X,y);
+		console.log("The number of updates is ", result.num_updates);
+		console.log("The last mistake index is ", result.last_mistake);
+		break;
+	case 16:
+	case 17:
+		results = []
+		n = argv.problem==16?1:0.5;
+		for(i=0;i<2000;i++){
+			//use i+1 as random seed
+			results.push(my_pla.random_cycle(X,y,i+1,n));
+			bar.tick();
+		}
+		console.log(results[1])
+		avg = _.reduce(results, function(sum, obj){
+			return sum + obj.num_updates;
+		},0) / 2000;
+
+		//draw histogram
+		filename = "problem"+argv.problem.toString()+".png";
+		var data = {
+		  x: _.map(results,function(obj){
+		  	return obj.num_updates;
+		  }),
+		  type: "histogram"
+		};
+		plotly.getImage({
+			data: [data]
+		} ,{
+			format: 'png'
+		},function(err, imageStream){
+			if(err)return console.log(err);
+			var fileStream = fs.createWriteStream(filename);
+    	imageStream.pipe(fileStream);
+    	console.log("The histogram is saved to",filename);
+		})
+
+		console.log("The average number of updates is ", avg);
+		break;
+	case 18:
+	case 19:
+		errors = [];
+		flag = argv.problem==19?true:false;
+		
+		for(i=0;i<2000;i++){
+			result = my_pla.pocket_alg(X,y,50,i+1,flag);
+			error = my_pla.error_rate(result.model,test_X,test_y);
+			errors.push(error);
+			bar.tick()
+		}
+		avg = _.reduce(errors, function(err,val){
+			return err+val;
+		},0) / 2000;
+
+		//draw histogram
+		filename = "problem"+argv.problem.toString()+".png";
+		var data = {
+		  x: errors,
+		  type: "histogram"
+		};
+		plotly.getImage({
+			data: [data]
+		} ,{
+			format: 'png'
+		},function(err, imageStream){
+			if(err)return console.log(err);
+			var fileStream = fs.createWriteStream(filename);
+    	imageStream.pipe(fileStream);
+    	console.log("The histogram is saved to",filename);
+		})
+		console.log("Average error rate is",avg)
+		break;
+
 		break;
 	default:
 		break;
--- hw1/pla.js
+++ hw1/pla.js
@@ -1,12 +1,17 @@
 var _ = require("underscore");
+var seedrandom = require("seedrandom");
 
 exports.naive_cycle = function(X, y){
 	var num=0, t, last_mistake;
 	N = X.length;
 	m = X[0].length;
 	w = Array.apply(null, Array(m)).map(Number.prototype.valueOf,0);
+	//create naive cycle
+	cycle = Array.apply(null, Array(N)).map(function(val,index){
+		return index;
+	})
 
-	while((t = mistake_index(w,X,y)) != -1){
+	while((t = mistake_index(w,X,y,cycle)) != -1){
 		num++;
 		last_mistake = t;
 		//console.log("iterator ", num, ": ");
@@ -16,12 +21,75 @@ exports.naive_cycle = function(X, y){
 		});
 	}
 
-	console.log("The number of updates is ", num);
-	console.log("The last mistake index is ", last_mistake);
+	return {
+		num_updates: num,
+		last_mistake: last_mistake
+	}
 }
 
-exports.random_cycle = function(X, y){
-	
+exports.random_cycle = function(X, y, seed, n){
+	var num=0, t, last_mistake;
+	N = X.length;
+	m = X[0].length;
+	w = Array.apply(null, Array(m)).map(Number.prototype.valueOf,0);
+	Math.seedrandom(seed);
+	//create random cycle
+	cycle = Array.apply(null, Array(N)).map(function(val,index){
+		return index;
+	})
+	random_cycle = _.shuffle(cycle);
+
+	while((t = mistake_index(w,X,y,random_cycle)) != -1){
+		num++;
+		last_mistake = t;
+		//console.log("iterator ", num, ": ");
+		//console.log("w: ", w)
+		w = _.map(w,function(wi, index){
+			return wi + n*y[t]*X[t][index];
+		})
+	}
+
+	return {
+		num_updates: num,
+		last_mistake: last_mistake
+	}
+}
+
+exports.pocket_alg = function(X,y,iter,seed,flag){
+	var num=0, t, last_mistake, w_pocket;
+	N = X.length;
+	m = X[0].length;
+	w0 = w_pocket = Array.apply(null, Array(m)).map(Number.prototype.valueOf,0);
+	ws = [w0];
+	Math.seedrandom(seed);
+	//create random cycle
+	cycle = Array.apply(null, Array(N)).map(function(val,index){
+		return index;
+	})
+	random_cycle = _.shuffle(cycle);
+
+	while(iter--){
+		num++;
+		t = mistake_index(ws[num-1],X,y,random_cycle);
+		last_mistake = t;
+		//console.log("iterator ", num, ": ");
+		//console.log("w: ", w)
+		w = _.map(ws[num-1],function(wi, index){
+			return wi + y[t]*X[t][index];
+		});
+		ws.push(w);
+		if(mistake_count(w,X,y) < mistake_count(w_pocket,X,y) || flag){
+			w_pocket = w;
+		}
+	}
+
+	return {
+		model: w_pocket
+	}
+}
+
+exports.error_rate = function(w,X,y){
+	return mistake_count(w,X,y) / X.length;
 }
 
 function sign(w, x){
@@ -31,10 +99,16 @@ function sign(w, x){
 	return h_x==1?1:-1;
 }
 
-function mistake_index(w,X,y){
+function mistake_index(w,X,y,cycle){
 	var i;
-	for(i = X.length-1;i >= 0;i--){
-		if(sign(w,X[i]) != y[i])break;
+	for(i = 0;i < cycle.length;i++){
+		index = cycle[i];
+		if(sign(w,X[index]) != y[index])return index;
 	}
-	return i;
+	return -1;
+}
+function mistake_count(w,X,y){
+	return _.reduce(X,function(cnt,val,i){
+		return cnt + (sign(w,val) != y[i]);
+	},0)
 }
